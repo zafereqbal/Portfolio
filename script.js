c00f23c2f01c95821b0eb29f57f50a434645fe06
zafereqbal <zafereqbal753@gmail.com>
2022-08-11 17:40:08 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+/*typing animation*/
+var typed = new Typed(".typing", {
+  strings: [
+    " ",
+    "Programmer",
+    " ",
+    "Competitive Coder",
+    " ",
+    "Web Developer",
+    " ",
+  ],
+  typeSpeed: 110,
+  BackSpeed: 70,
+  loop: true,
+});
